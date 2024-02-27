071b6eb9e92e7c5810e7de583c6f43d45a8a4f8a
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-02-26 19:37:58 -0600

    add presentation, praise to reveal.js

---
+++ src/routes/presentation/Code.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let id = null
+	export let lines = null
+	export let noescape = false
+</script>
+
+<pre data-id={id || null}>
+  <code
+    data-trim
+    data-line-numbers={lines || null}
+    data-noescape={noescape || null}
+  >
+    <slot />
+  </code>
+</pre>
