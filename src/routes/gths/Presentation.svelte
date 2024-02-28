cc349ab580286adfdb47fd3c2f267bee56ed3c7e
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-02-28 15:09:23 -0600

    Update Presentation.svelte

--- src/routes/gths/Presentation.svelte
+++ src/routes/gths/Presentation.svelte
@@ -3,13 +3,15 @@
 	<p>Jerry Wells, K-12</p>
 </section>
 <section>
+	<h1>Why identify in HS</h1>
+	<p>Gifted Underachievement</p>
+	<a href="https://www.davidsonacademy.unr.edu/blog/underachievement-in-gifted-students/">https://www.davidsonacademy.unr.edu/blog/underachievement-in-gifted-students/</a>
+	<p>Another resource while navigating the MS to HS to college/career process</p>
+	<p>Looking to include GT during advisory for these conversations (currently Friday)</p>
+</section>
+<section>
 	<h1>Referral</h1>
 	<p>Identify in 3rd, but relevant throughout</p>
 	<p>Form available at the GT page:</p>
 	<a href="https://www.eurekaspringsschools.k12.ar.us/page/gifted-and-talented">https://www.eurekaspringsschools.k12.ar.us/page/gifted-and-talented</a>
-	<p>Gives students another resource, ideally a backstop against "gifted underachievement" and other issues</p>
-</section>
-<section>
-	<h1>Gifted Underachievement</h1>
-	<a href="https://www.usnews.com/education/k12/articles/gifted-and-talented-programs-what-parents-should-know">https://www.usnews.com/education/k12/articles/gifted-and-talented-programs-what-parents-should-know</a>
 </section>
