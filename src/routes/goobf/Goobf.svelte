9669568060272fd3085c61e2ed29413383f4e058
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-03-05 14:04:21 -0600

    decthulize goobf (somewhat)

--- src/routes/goobf/Goobf.svelte
+++ src/routes/goobf/Goobf.svelte
@@ -16,30 +16,25 @@
     
         show() {
             let index = this.p5.floor(this.index) % this.len;
-            this.p5.image(this.animation[index], this.x, this.y);
+            this.p5.image(this.animation[index], this.x, this.y, 500, 500);
         }
     
         animate() {
             this.index += this.speed;
-            this.x += this.speed * 15;
-    
-            if (this.x > this.p5.width) {
-                this.x = -this.w;
-            }
         }
     }
 
     const sketch = (p5) => {
         let x = 50;
         let y = 50.0;  // velocity?
-        let speed = 3; // speed
+        let speed = 2; // speed
         let radius = 40.0; // size of radius of the circle
         let direct = -1;//direction of movement
         let r = 25;
         let hit = false;
         let count = 2;
         let animation = [];
-        let goobfs = [];
+        let goobf;
         let spritedata;
         let spritesheet;
 
@@ -49,35 +44,30 @@
         }
 
         p5.setup = () => {
-            p5.createCanvas(600, 600);
+            p5.createCanvas(1300, 1300);
             let frames = spritedata.frames;
             for (let i = 0; i < frames.length; i++) {
                 let pos = frames[i].position;
                 let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
                 animation.push(img);
             }
-            for (let i = 0; i < 5; i++) {
-                goobfs[i] = new Sprite(animation, 0, i * 75, p5.random(0.1, 0.4), p5);
-            }
+            goobf = new Sprite(animation, 0, 75, 0.05, p5);   
         }
 
         p5.draw = () => {
             p5.background(42,42,42);
-            for (let goobf of goobfs) {
-                goobf.show();
-                goobf.animate();
-            }
+            goobf.show();
+            goobf.animate();
             p5.point(p5.mouseX, p5.mouseY);
             hit = collidePointCircle(p5.mouseX, p5.mouseY, x, y, 100);
             p5.fill(hit ? p5.color('#2196F3') : 255);
-            console.log('colliding?', hit, count);
             if (hit) {
                 count++;
             }
 
             p5.ellipse(x, y, r*2, r*2);
             y += speed * direct;
-            if ((y > p5.height - radius)  || (y < radius))  {
+            if ((y > p5.height/2 - radius) || (y < radius))  {
                 direct = - direct;  //change direction
             }
             p5.textSize(100);
