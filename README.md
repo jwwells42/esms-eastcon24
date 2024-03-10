ff6fcd8b8528fc5387c7a4fedae924e7337fd789
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-03-10 16:21:18 +0000

    update readme, add accessibility

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # EASTCON24
 
 This repository is a collection of our greatest hits in EAST in the 2023-2024 school year. We are a work in progress--be kind!
+
+# Build, test, contribute
+
+git clone https://github.com/jwwells42/esms-eastcon24
+cd esms-eastcon24
+npm install
+
+To watch for changes and test locally:
+
+npm run dev
+
+To see if it builds:
+
+npm run build
+
--- src/routes/accessibility/+page.svelte
+++ src/routes/accessibility/+page.svelte
@@ -2,4 +2,4 @@
     <title>Downtown Eureka Springs Accessibility</title>
 </svelte:head>
 
-UNDER CONSTRUCTION
+<iframe name="survey123webform" width="500" height="1000" frameborder="0" marginheight="0" marginwidth="0" title="My Survey" src="https://survey123.arcgis.com/share/7e81a8f252af4228b9b1d838468d3ac0" allow="geolocation https://survey123.arcgis.com; camera https://survey123.arcgis.com"></iframe>
