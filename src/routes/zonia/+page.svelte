338bbc1e7866c4c62d23b1c4ba9ee99dd026a5ad
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-03-10 16:39:52 +0000

    add pov to main zonia header entry

--- src/routes/zonia/+page.svelte
+++ src/routes/zonia/+page.svelte
@@ -1,9 +1,10 @@
 <script>
     import Globe from './Globe.svelte';
+    let pov = [45, -97, 0.5];
 </script>
 
 <svelte:head>
     <title>Zonia</title>
 </svelte:head>
 
-<Globe chosenJSON="/globeData/us.json" />
+<Globe {pov} chosenJSON="/globeData/us.json" />
