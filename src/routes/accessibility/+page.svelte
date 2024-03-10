9a673f6ebb6212ec18e62317495e09d91bd0598a
jwwells42 <63275941+jwwells42@users.noreply.github.com>
2024-03-10 16:38:21 +0000

    add accessibility iframes

--- src/routes/accessibility/+page.svelte
+++ src/routes/accessibility/+page.svelte
@@ -2,4 +2,13 @@
     <title>Downtown Eureka Springs Accessibility</title>
 </svelte:head>
 
-<iframe name="survey123webform" width="500" height="1000" frameborder="0" marginheight="0" marginwidth="0" title="My Survey" src="https://survey123.arcgis.com/share/7e81a8f252af4228b9b1d838468d3ac0" allow="geolocation https://survey123.arcgis.com; camera https://survey123.arcgis.com"></iframe>
+<div class="flex">
+    <iframe name="survey123webform" width="500" height="1000" frameborder="0" marginheight="0" marginwidth="0" title="My Survey" src="https://survey123.arcgis.com/share/7e81a8f252af4228b9b1d838468d3ac0" allow="geolocation https://survey123.arcgis.com; camera https://survey123.arcgis.com"></iframe>
+    <iframe src="https://east.maps.arcgis.com/apps/instant/basic/index.html?appid=30115914f0fc40e28150f6d12e37099a" width="2000" height="800" frameborder="0" style="border:0" allowfullscreen>iFrames are not supported on this page.</iframe>
+</div>
+
+<style>
+    .flex {
+        display: flex;
+    }
+</style>
